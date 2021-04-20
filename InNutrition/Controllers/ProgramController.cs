0c8b0aff5b1dd023600805ec06626057e8bb10b0
idesignerse <heberandrade@gmail.com>
2021-04-20 09:13:53 +0200

    Creating IActionResult
    Program Controller
    IActionResult Program
    IActionResult Details

---
+++ InNutrition/Controllers/ProgramController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InNutrition.Controllers
+{
+    public class ProgramController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+
+        public IActionResult Program(int id)
+        {
+            return Ok("This IActionResult Program id is " + id);
+        }
+
+        public IActionResult Details(int id)
+        {
+            return Ok("This IActionResult Details id is " + id);
+        }
+    }
+}
