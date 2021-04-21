9df7825a2b4540bb601c8d19f03289f66bb81050
idesignerse <heberandrade@gmail.com>
2021-04-21 11:09:41 +0200

    comments only

--- InNutrition/Controllers/ProgramHabitController.cs
+++ InNutrition/Controllers/ProgramHabitController.cs
@@ -9,6 +9,7 @@ namespace InNutrition.Controllers
     public class ProgramHabitController : Controller
     {
         [Route("ProgramHabit/Index/{id?}")]
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
@@ -19,6 +20,38 @@ namespace InNutrition.Controllers
         {
             return Ok("This IActionResult Program id is " + id);
         }
+
+        // OTHERS EXEMPLES From a query
+        //public IActionResult GetDurationData([FromQuery] string values)
+        //{
+        //    return Ok("The value : " + value + " is from Query string);
+        //}
+
+        //WITH POSTMAN
+        // using POSTMAN this mostly use with APIs
+        // [HttpPost]
+        //public IActionResult Post([FromHeader] string parentrequestId)
+        //{
+        //    return Ok($"Got a header with parentRequestId: " {parentRequestId}!" );
+        //}
+
+        //Another exemple Request Body
+        // using POSTMAN this mostly use with APIs
+        // [HttpPost]
+        //public IActionResult AddDetails([FromBody] ProgramHabit program)
+        //{
+        //   return Ok(program);
+        //}
+
+        //Another exemple PASS PARAMEMETERS in a form
+        // using POSTMAN this mostly use with APIs
+        // 
+        // [HttpPost]
+        //public IActionResult SaveFile([FromForm] string filename, [FromForm] IFormFile file)
+        //{
+        //    return Ok("Success);
+        //}  
+
 
         public IActionResult Details(int id)
         {
